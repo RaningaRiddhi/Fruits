9efefdcb4bc8ba5a53a01445f4df84a660626b39
riddhi raninga <soniriddhi2002@com>
2022-04-28 10:31:54 +0530

    added folder

---
+++ lib/constants/constants.dart
@@ -0,0 +1,17 @@
+class Constants{
+  static const String POPPINS = "Poppins";
+  static const String OPEN_SANS = "OpenSans";
+  static const String SKIP = "Skip";
+  static const String NEXT = "Next";
+  static const String SLIDER_HEADING_1 = "Search";
+  static const String SLIDER_HEADING_2 = "View and Update";
+  static const String SLIDER_HEADING_3 = "Deals";
+  static const String SLIDER_HEADING_4 = "Products";
+
+  static const String SLIDER_DESC1 = "Search how to make videos and solution to quickly troubleshoot and enchance performance";
+  static const String SLIDER_DESC2 = "View and update warranty status and local service providers and check repair status";
+  static const String SLIDER_DESC3 = "Check out our deals on Lenovo products in the Shop section";
+  static const String SLIDER_DESC4 = "Your Lenovo products and how to optimize them by accessing product manuals";
+
+
+}
